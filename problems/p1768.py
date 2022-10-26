e972f50fee90329e04da70d90f74cb35617d340a
mole828 <mrx800@163.com>
2022-10-26 19:09:42 +0800

    change 1768

--- problems/p1768.py
+++ problems/p1768.py
@@ -2,8 +2,8 @@ from itertools import zip_longest
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ans = []
+        ans = ''
         for x, y in zip_longest(word1, word2):
-            if x: ans.append(x)
-            if y: ans.append(y)
-        return ''.join(ans)
+            if x: ans+=x
+            if y: ans+=y
+        return ans
