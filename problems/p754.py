ef5899ea7902d4d62e76059df404f60575a90894
mole828 <mrx800@163.com>
2022-11-04 11:17:59 +0800

    daily

---
+++ problems/p754.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        sum = 0
+        i = 1
+        while True:
+            sum += i
+            if sum>= target and (sum - target)%2==0:
+                return i
+            i += 1
