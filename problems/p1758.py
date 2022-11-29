99c992c3d72fae2abba2c316e66f21ebb4d41c26
mole828 <mrx800@163.com>
2022-11-29 12:28:07 +0800

    daily

---
+++ problems/p1758.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def minOperations(self, s: str) -> int:
+        cnt = sum(int(c) != i % 2 for i, c in enumerate(s))
+        return min(cnt, len(s) - cnt)
+
+
+if __name__ == '__main__':
+    Solution().minOperations("0100")
