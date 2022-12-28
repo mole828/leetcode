1e369c4dfa00c7dae245de2427e0386cd8c4819d
mole828 <mrx800@163.com>
2022-12-28 10:06:14 +0800

    daily

---
+++ problems/p1750.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l, r = 0, len(s) - 1
+        while l < r and s[l] == s[r]:
+            while l <= r and s[l] == s[r]: l += 1
+            while l < r and s[l - 1] == s[r]: r -= 1
+        return r - l + 1
