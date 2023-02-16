6f3fd6b8e62899dbba4a1b1cda86b1b242b396c7
mole828 <mrx800@163.com>
2023-02-16 11:28:17 +0800

    改用numpy相加

--- problems/p2341.py
+++ problems/p2341.py
@@ -1,11 +1,17 @@
 from collections import Counter
+from functools import reduce
 from typing import List
 
 
+import numpy as np
+
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
-        counter = Counter(nums)
-        return [
-            sum(x//2 for x in counter.values()), 
-            sum(x%2 for x in counter.values())
-        ]
+        return reduce(
+            lambda a,b:a+b,
+            [np.array([x//2,x%2]) for x in Counter(nums).values()]
+        ).tolist()
+
+
+if __name__ == '__main__':
+    print(Solution().numberOfPairs(nums = [1,3,2,1,3,2,2]))
