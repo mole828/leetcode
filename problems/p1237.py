1ac1fe84fe1039df6662984d80840a0c4b4cef6a
mole828 <mrx800@163.com>
2023-02-18 00:11:28 +0800

    daily

---
+++ problems/p1237.py
@@ -0,0 +1,15 @@
+
+class CustomFunction:
+    def f(self, x, y)->int:pass
+
+class Solution:
+    def findSolution(self, customfunction: CustomFunction, z: int) -> List[List[int]]:
+        y, g = z, customfunction.f
+        for x in range(1, z + 1):
+            while y:
+                d = g(x, y) - z
+                if d <= 0:
+                    if not d:
+                        yield [x, y]
+                    break
+                y -= 1
