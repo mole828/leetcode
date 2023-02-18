d009d2c4c5219fa87823db2c16c603a8532a6df8
mole828 <mrx800@163.com>
2023-02-19 02:07:18 +0800

    优化有序列表为最小堆

--- problems/p1792.py
+++ problems/p1792.py
@@ -1,17 +1,18 @@
 from typing import List
-from sortedcontainers import SortedList
+import heapq
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         def dr(nPass,nSum):
             return (nPass+1)/(nSum+1) - nPass/nSum
-        classes = SortedList([[nPass, nSum, dr(nPass,nSum)] for (nPass, nSum) in classes], key=lambda i:i[2])
+        classes = [(-dr(nPass,nSum), nPass, nSum, ) for (nPass, nSum) in classes]
+        heapq.heapify(classes)
         for _ in range(extraStudents):
-            [nPass, nSum, _] = classes.pop()
+            print(classes)
+            [_, nPass, nSum, ] = heapq.heappop(classes)
             nPass += 1
             nSum += 1 
-            classes.add([nPass,nSum,dr(nPass,nSum)])
-        
-        return sum( nPass/nSum for [nPass,nSum,_] in classes ) / len(classes)
+            heapq.heappush(classes, (-dr(nPass,nSum),nPass,nSum,) )
+        return sum( nPass/nSum for [_,nPass,nSum,] in classes ) / len(classes)
 
 if __name__ == '__main__':
     Solution().maxAverageRatio(classes = [[1,2],[3,5],[2,2]], extraStudents = 2)
