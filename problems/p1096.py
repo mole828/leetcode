6759bfba6dc0b3a501f05c035aec4e59fdc397a2
mole828 <mrx800@163.com>
2023-03-07 23:45:12 +0800

    daily

---
+++ problems/p1096.py
@@ -0,0 +1,11 @@
+import itertools
+from typing import List
+
+
+class Solution:
+    def braceExpansionII(self, expression: str) -> List[str]:
+        class S(set):
+            __add__ = lambda s, o: S(s | o)
+            __mul__ = lambda s, o: S(i + j for i, j in itertools.product(s, o))
+        expression = re.sub('(\w+)', r'S(["\1"])', expression).replace(',', '+').replace('{', '(').replace('}', ')').replace(')(',')*(').replace(')S',')*S')
+        return sorted(S(eval(expression)))
