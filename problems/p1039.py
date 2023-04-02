09f686ec7af90aa82f2e20d05c3d8e690bbf2ab0
mole828 <mrx800@163.com>
2023-04-02 21:50:40 +0800

    daily

---
+++ problems/p1039.py
@@ -0,0 +1,13 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if i + 1 == j:
+                return 0
+            return min(dfs(i, k) + dfs(k, j) + values[i] * values[k] * values[j] for k in range(i + 1, j))
+
+        return dfs(0, len(values) - 1)
