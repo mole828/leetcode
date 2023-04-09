b70db35a178f5eeb602592a47500117f0c14b6a4
mole828 <mrx800@163.com>
2023-04-09 22:28:55 +0800

    daily

---
+++ problems/p2399.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+        d = defaultdict(int)
+        for i, c in enumerate(s, 1):
+            if d[c] and i - d[c] - 1 != distance[ord(c) - ord('a')]:
+                return False
+            d[c] = i
+        return True
