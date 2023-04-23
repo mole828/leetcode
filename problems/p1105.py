43d67d66543038c164a5b6762306ab8a1d30382e
mole828 <mrx800@163.com>
2023-04-23 22:34:27 +0800

    daily

---
+++ problems/p1105.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:
+        n = len(books)
+        dp = [1000000] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            tmp_width, j, h = 0, i, 0
+            while j > 0:
+                tmp_width += books[j - 1][0]
+                if tmp_width > shelf_width:
+                    break
+                h = max(h, books[j - 1][1])
+                dp[i] = min(dp[i], dp[j - 1] + h)
+                j -= 1
+        return dp[-1]
