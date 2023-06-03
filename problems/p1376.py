63dfadb6af39b38cea6d40cc86148be7612042bd
mole828 <mrx800@163.com>
2023-06-03 10:16:36 +0800

    daily

--- problems/p1376.py
+++ problems/p1376.py
@@ -1,6 +1,9 @@
 from functools import cache
 from typing import List
 
+'''
+2023.06.03
+'''
 
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
