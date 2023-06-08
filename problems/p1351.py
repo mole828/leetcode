4e991eb3a8f78c7e5a599f61ebc5c4dc86ef4d98
mole828 <mrx800@163.com>
2023-06-08 11:14:21 +0800

    fix ans

--- problems/p1351.py
+++ problems/p1351.py
@@ -3,9 +3,16 @@ from typing import List
 
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        ans = 0 
-        for row in grid:
-            for x in row:
-                if x<0:
-                    ans+=1
+        ans = 0
+        y = len(grid) - 1
+        x = 0
+        while y >= 0:
+            ly = len(grid[y])
+            while x != ly:
+                if grid[y][x] < 0:
+                    break
+                x += 1
+            # print(f"ans+= {n-x}", y, x)
+            ans += ly - x
+            y -= 1
         return ans
