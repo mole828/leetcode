dcf0973f2c6271fc827113a35de2e01776d7ca98
mole828 <mrx800@163.com>
2023-09-17 16:34:18 +0800

    update

---
+++ problems/847.shortest-path-visiting-all-nodes.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=847 lang=python3
+#
+# [847] Shortest Path Visiting All Nodes
+#
+
+# @lc code=start
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        queue = deque([(1 << i, i, 0) for i in range(n)])
+        visited = set((1 << i, i) for i in range(n))
+        
+        while queue:
+            mask, node, dist = queue.popleft()
+            if mask == (1 << n) - 1:
+                return dist
+            for neighbor in graph[node]:
+                new_mask = mask | (1 << neighbor)
+                if (new_mask, neighbor) not in visited:
+                    visited.add((new_mask, neighbor))
+                    queue.append((new_mask, neighbor, dist + 1))
+# @lc code=end
+
