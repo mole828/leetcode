7828a39c4488309a43b1a9cfed969ceda4df306d
mole828 <mrx800@163.com>
2023-09-24 17:36:19 +0800

    update

---
+++ problems/799.champagne-tower.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=799 lang=python3
+#
+# [799] Champagne Tower
+#
+
+# @lc code=start
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        row = [poured]
+        for y in range(1, query_row+1):
+            next_row = [0]*(y+1)
+            for j, volume in enumerate(row):
+                if volume > 1:
+                    next_row[j] += (volume - 1) / 2
+                    next_row[j + 1] += (volume - 1) / 2
+            row = next_row
+            
+        return min(1, row[query_glass])
+# @lc code=end
+
