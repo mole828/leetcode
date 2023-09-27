7ee74faa4173ccba5d2fe19c5f620adbb8d7b983
mole828 <mrx800@163.com>
2023-09-27 09:35:23 +0800

    del typescript

--- problems/2635.apply-transform-over-each-element-in-array.ts
+++
@@ -1,7 +0,0 @@
-function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const result: number[] = [];
-    arr.forEach((value, index)=>{
-        result.push(fn(value, index))
-    })
-    return result;
-};
