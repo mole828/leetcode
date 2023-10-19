5467fe3c6c716319aa695d7ed60b8ced38ddf7da
mole828 <mrx800@163.com>
2023-10-19 10:37:02 +0800

    daily

---
+++ problems/844.backspace-string-compare.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=844 lang=python3
+#
+# [844] Backspace String Compare
+#
+
+# @lc code=start
+import regex
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def f(line: str):
+            ans = []
+            for c in line:
+                if c == '#':
+                    if ans:
+                        ans.pop()
+                else:
+                    ans.append(c)
+            return ''.join(ans)
+        return f(s) == f(t)
+# @lc code=end
+
