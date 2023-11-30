f5e7193dd01a7c5510dc802437516671adce9503
mole828 <mrx800@163.com>
2023-11-30 17:15:44 +0800

    daily

---
+++ problems/1611.minimum-one-bit-operations-to-make-integers-zero.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=1611 lang=python3
+#
+# [1611] Minimum One Bit Operations to Make Integers Zero
+#
+
+# @lc code=start
+from functools import cache
+import math
+
+
+@cache
+def bto0(b): 
+    return (1<<b+1) - 1
+
+@cache
+def nto0(n):
+    if n == 0: return 0
+    b1 = int(math.log2(n))
+    return bto0(b1) - nto0(n - (1<<b1))
+
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        return nto0(n)
+# @lc code=end
+
+print(Solution().minimumOneBitOperations(9))
