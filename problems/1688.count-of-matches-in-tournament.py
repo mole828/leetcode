ea70b83508a6ac8219a2811e4990ac83224c717c
mole828 <mrx800@163.com>
2023-12-05 10:05:18 +0800

    daily

---
+++ problems/1688.count-of-matches-in-tournament.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1688 lang=python3
+#
+# [1688] Count of Matches in Tournament
+#
+
+# @lc code=start
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+        
+# @lc code=end
+
