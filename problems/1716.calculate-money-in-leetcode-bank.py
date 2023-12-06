498a117bb0ce599b812e30385c4e3d0f8fe28b06
mole828 <mrx800@163.com>
2023-12-06 10:26:32 +0800

    daily

---
+++ problems/1716.calculate-money-in-leetcode-bank.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1716 lang=python3
+#
+# [1716] Calculate Money in Leetcode Bank
+#
+
+# @lc code=start
+
+# HACK: 可以优化
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        output = 0
+        for week in range(1,200):
+            for day in range(7):
+                output += week+day 
+                n -= 1
+                if not n :
+                    return output
+        return 0
+
+# @lc code=end
+
