2174413af4d0177a8311aa863a55c80bcdc37aa5
mole828 <mrx800@163.com>
2023-12-16 11:29:14 +0800

    daily

---
+++ problems/242.valid-anagram.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
+# @lc code=end
+
