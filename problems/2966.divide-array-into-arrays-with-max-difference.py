d61180a1708b52e1c262d09b258ea003014d7a10
mole828 <mrx800@163.com>
2024-02-01 09:06:08 +0800

    daily

---
+++ problems/2966.divide-array-into-arrays-with-max-difference.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=2966 lang=python3
+#
+# [2966] Divide Array Into Arrays With Max Difference
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        answer:List[List[int]] = []
+        box = []
+        while nums:
+            i = nums.pop() 
+            box.append(i)
+            if len(box) == 3:
+                if max(box)-min(box) > k:
+                    return []
+                answer.append(box)
+                box = [] 
+        return answer
+# @lc code=end
+
+# 烂题
