42caf6a1add5493cdd9f3467c39007c6a7bf5d9c
mole828 <mrx800@163.com>
2024-04-17 09:17:29 +0800

    Revert "fix: treenode"
    This reverts commit 14b0e6a3a6ab4fb346e75f6dfe2dbb491ea68df5.

--- problems/tools/TreeNode.py
+++ problems/tools/TreeNode.py
@@ -1,57 +1,20 @@
 
-from typing import Optional, TypeVar, Generic
-import warnings
-from deprecated.sphinx import deprecated
+from typing import Optional
 
-warnings.filterwarnings('always')
 
-T = TypeVar('T', int, str)
-
-class TreeNode(Generic[T]):
-    '''
-    simple use TreeNode
-    '''
-    val: T
-    left: 'TreeNode[T]'
-    right: 'TreeNode[T]'
-    def __init__(self, val:T=0, left=None, right=None):
+class TreeNode: 
+    val: int 
+    left: 'TreeNode' 
+    right: 'TreeNode' 
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
     def __str__(self) -> str:
         return f"TreeNode({self.val})[{self.left},{self.right}]"
-    
-    @staticmethod
-    def build(_array: list[Optional[T]]) -> Optional['TreeNode[T]']:
-        '''
-        build a Tree
-        '''
-        if len(_array) == 0:
-            return None
-        # print(array)
-        root = TreeNode(_array.pop(0))
-        que = [root]
-        while _array:
-            parent = que.pop(0)
-            # print(root.val)
-            v_left = _array.pop(0)
-            if v_left:
-                left = TreeNode(v_left)
-                parent.left = left
-                que.append(left)
-            if _array:
-                v_right = _array.pop(0)
-                if v_right:
-                    right = TreeNode(v_right)
-                    parent.right = right
-                    que.append(right)
-            # print(parent)
-        return root
 
-@deprecated(reason="use TreeNode.build")
 class TreeBuilder:
-    @deprecated(reason="use TreeNode.build")
     def build(array: list[Optional[int]]) -> Optional[TreeNode]:
         if len(array) == 0:
             return None 
