46c6fbf5ea3eab805b6c094d9e7246461b57012b
mole828 <mrx800@163.com>
2024-06-12 09:22:13 +0800

    daily

---
+++ problems/75.sort-colors.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=75 lang=python3
+# @lcpr version=
+#
+# [75] Sort Colors
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [2,0,2,1,1,0]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [2,0,1]\n
+# @lcpr case=end
+
+#
+
