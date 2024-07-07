eaf7435dbc03f7643024f646ffe1663d8a9e1ce3
mole828 <mrx800@163.com>
2024-07-07 09:18:23 +0800

    daily

---
+++ problems/1518.water-bottles.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=1518 lang=python3
+# @lcpr version=
+#
+# [1518] Water Bottles
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        drink = 0
+        full_bottles = numBottles
+        empty_bottles = 0
+        while full_bottles:
+            drink += full_bottles
+            empty_bottles += full_bottles
+            full_bottles = 0
+
+            full_bottles += empty_bottles // numExchange
+            empty_bottles %= numExchange
+
+        return drink
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# 9\n3\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 15\n4\n
+# @lcpr case=end
+
+#
+
