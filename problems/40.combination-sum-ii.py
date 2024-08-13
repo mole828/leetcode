cce45f7972bc9034329d06a1c23ed7a60cb395ec
mole828 <mrx800@163.com>
2024-08-13 23:26:05 +0800

    daily

---
+++ problems/40.combination-sum-ii.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=40 lang=python3
+# @lcpr version=
+#
+# [40] Combination Sum II
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        ans, c = [], Counter(candidates)
+
+        def dfs(s, n, res):
+            if s == target: return ans.append(res)
+            if s > target or n < 1: return
+            for i in range(0, c[n]+1): dfs(s+i*n, n-1, res+[n]*i)
+
+        dfs(0, 50, [])
+        return ans
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [10,1,2,7,6,1,5]\n8\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [2,5,2,1,2]\n5\n
+# @lcpr case=end
+
+#
+
