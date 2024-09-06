1938fe604428f011f773940733957ac7f93923e4
mole828 <mrx800@163.com>
2024-09-06 09:58:23 +0800

    daily

---
+++ problems/3217.delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=3217 lang=python3
+# @lcpr version=
+#
+# [3217] Delete Nodes From Linked List Present in Array
+#
+
+from tools.ListNode import ListNode
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import List, Optional
+
+
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = set(nums)
+        begin = ListNode()
+        begin.next = head
+        this = begin
+        while this.next:
+            if this.next.val in nums:
+                this.next = this.next.next
+            else:
+                this = this.next
+        return begin.next
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,2,3]\n[1,2,3,4,5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n[1,2,1,2,1,2]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [5]\n[1,2,3,4]\n
+# @lcpr case=end
+
+#
+
