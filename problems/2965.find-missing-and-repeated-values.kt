29b08ddc15da2f33f1ca6fe2e2948ca2578948fa
mole828 <mrx800@163.com>
2025-03-06 11:35:14 +0800

    daily

---
+++ problems/2965.find-missing-and-repeated-values.kt
@@ -0,0 +1,25 @@
+package p2965
+/*
+ * @lc app=leetcode id=2965 lang=kotlin
+ *
+ * [2965] Find Missing and Repeated Values
+ */
+
+
+// @lc code=start
+class Solution {
+    fun findMissingAndRepeatedValues(grid: Array<IntArray>): IntArray {
+        val gridSize = grid.sumOf({it.size})
+        val nums = grid.flatMap { it.toList() }
+        val hasSet = nums.toSet()
+        val shouldSet = (1..gridSize).toSet()
+        val missing = shouldSet.minus(hasSet).first()
+        val repeated = nums.groupBy { it }.filter { it.value.size > 1 }.keys.first()
+        return intArrayOf(repeated, missing)
+    }
+}
+// @lc code=end
+
+fun main() {
+    println(Solution().findMissingAndRepeatedValues(arrayOf(intArrayOf(1, 3), intArrayOf(2, 2))))
+}
