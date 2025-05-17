f9754be524422459f9d5af2e9a208540737664c7
mole828 <mrx800@163.com>
2025-05-18 02:03:27 +0800

    daily

--- problems/75.sort-colors.py
+++ problems/75.sort-colors.py
@@ -17,6 +17,30 @@ class Solution:
         """
         nums.sort()
         
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c0, c1, c2 = 0, 0, 0
+        for x in nums:
+            if x == 0:
+                c0 += 1
+            elif x == 1:
+                c1 += 1
+            else:
+                c2 += 1
+        i = 0
+        for _ in range(c0):
+            nums[i] = 0
+            i += 1
+        for _ in range(c1):
+            nums[i] = 1
+            i += 1
+        for _ in range(c2):
+            nums[i] = 2
+            i += 1
+
 # @lc code=end
 
 
