124a992695d4e77cac19380989e0f71e70ac6777
mole828 <mrx800@163.com>
2025-05-23 09:43:41 +0800

    daily

--- problems/3068.find-the-maximum-sum-of-node-values.py
+++ problems/3068.find-the-maximum-sum-of-node-values.py
@@ -10,13 +10,12 @@
 
 # @lcpr-template-end
 # @lc code=start
-import heapq
 from typing import List
 
 
 class Solution:
     # Time Limit Exceeded, 353/717 cases passed (N/A)
-    def maximumValueSum(self, nums: List[int], k: int, edges: List[tuple[int,int]]) -> int:
+    def _maximumValueSum(self, nums: List[int], k: int, edges: List[tuple[int,int]]) -> int:
         def mask2nums(mask: int) -> list[int]:
             i = 0
             new_nums = nums.copy()
@@ -55,7 +54,7 @@ class Solution:
 
         if count % 2 == 0:
             return totalSum
-        return max(totalSum - positiveMin, totalSum + negativeMax)
+        return int(max(totalSum - positiveMin, totalSum + negativeMax))
 
 
 # @lc code=end
