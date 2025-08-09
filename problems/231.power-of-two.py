0693c7f99462fc6c2b70c3cf23a631dc8c99d1f1
mole828 <mrx800@163.com>
2025-08-09 09:05:35 +0800

    daily

--- problems/231.power-of-two.py
+++ problems/231.power-of-two.py
@@ -4,18 +4,30 @@
 # [231] Power of Two
 #
 
+
+# @lcpr-template-start
+
+# @lcpr-template-end
 # @lc code=start
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         print(n)
-        if n is 1:return True
+        if n == 1:return True
         if n<0 or n==0 or n%2!=0:return False
         return self.isPowerOfTwo(n//2)
     
-class Solution(object):
-    def isPowerOfTwo(self, n):
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
         return n and not (n & n - 1)
+    
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return n.bit_count() == 1
         
 # @lc code=end
 
-Solution().isPowerOfTwo(16)
+print(Solution().isPowerOfTwo(16))
+print(Solution().isPowerOfTwo(-16))
+
