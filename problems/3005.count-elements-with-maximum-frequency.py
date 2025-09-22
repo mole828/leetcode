1e19f332372feb5a9340f2da369bf1de8c0bc1a5
mole828 <mrx800@163.com>
2025-09-22 09:29:33 +0800

    daily

---
+++ problems/3005.count-elements-with-maximum-frequency.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=3005 lang=python3
+#
+# [3005] Count Elements With Maximum Frequency
+#
+
+# @lc code=start
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        most_common = counter.most_common()
+        count_need = most_common[0][1] 
+        freq = 0
+        for _,count in most_common:
+            if count != count_need:
+                break
+            freq += count 
+        return freq
+# @lc code=end
+
