050db49eca02c450fc60d7cb3e28943231ae2a90
mole828 <mrx800@163.com>
2025-11-02 09:52:24 +0800

    daily

--- problems/2257.count-unguarded-cells-in-the-grid.py
+++ problems/2257.count-unguarded-cells-in-the-grid.py
@@ -17,11 +17,16 @@ from typing import List
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         mat = [[0] * n for _ in range(m)]
+
         for x, y in walls:
             mat[x][y] = 2
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
         for gx, gy in guards:
             mat[gx][gy] = 2
+        
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        for gx, gy in guards:
             for dx, dy in directions:
                 x, y = gx, gy
                 while 0 <= x + dx < m and 0 <= y + dy < n and mat[x + dx][y + dy] != 2:
@@ -29,9 +34,6 @@ class Solution:
                     x += dx
                     y += dy
         return sum(row.count(0) for row in mat)
-
-# https://leetcode.com/problems/count-unguarded-cells-in-the-grid/solutions/6067031/beats-100-simple-and-easy-i-guess-list-most-common-array-interview-problems/
-# 为什么我的超时 他这个代码能过呢
 
 # @lc code=end
 
