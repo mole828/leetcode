960a92efe53149a2b84b2c046515c4291ffd7265
mole828 <mrx800@163.com>
2025-12-12 17:13:54 +0800

    Update 1.two-sum.py

--- problems/1.two-sum.py
+++ problems/1.two-sum.py
@@ -5,18 +5,22 @@
 #
 
 # @lc code=start
-from typing import List,Dict
+from typing import Dict, List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        has:Dict[int, int] = {}
+        has: Dict[int, int] = {}
         for i in range(len(nums)):
             num = nums[i]
             if num in has:
                 return [has[num], i]
             need = target - num
             has[need] = i
+        raise ValueError("No two sum solution")
+
 
 # @lc code=end
 
+if __name__ == "__main__":
+    print(Solution().twoSum([2, 7, 11, 15], 9))
