8a72ab9438d24bc3dc956909f412eef7c0606947
mole828 <mrx800@163.com>
2026-01-09 10:19:50 +0800

    daily

---
+++ problems/865.smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=865 lang=python3
+#
+# [865] Smallest Subtree with all the Deepest Nodes
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node: Optional[TreeNode]) -> tuple[int, Optional[TreeNode]]:
+            """
+            returns (max_depth, min_subtree_root)
+            """
+            if not node:
+                return (0, None)
+            left_depth, left_node = dfs(node.left)
+            right_depth, right_node = dfs(node.right)
+            if left_depth > right_depth:
+                return (left_depth + 1, left_node)
+            elif right_depth > left_depth:
+                return (right_depth + 1, right_node)
+            else:
+                return (left_depth + 1, node)
+
+        _, result = dfs(root)
+        return result
+
+# @lc code=end
+
