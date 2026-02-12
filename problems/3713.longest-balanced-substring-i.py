26404f08391a00320944cbf99a67ae64a38bfaa9
mole828 <mrx800@163.com>
2026-02-12 13:55:32 +0800

    daily

---
+++ problems/3713.longest-balanced-substring-i.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=3713 lang=python3
+#
+# [3713] Longest Balanced Substring I
+#
+
+# @lc code=start
+from collections import Counter
+
+
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        ans = 1
+        n = len(s)
+        for length in range(n, 0, -1):
+            for left in range(0, n - length + 1):
+                sub = s[left : left + length]
+                count = Counter(sub)
+                c_set = set(v for v in count.values())
+                # print(sub, count, c_set)
+                if len(c_set) == 1:
+                    ans = max(ans, length)
+                    break
+        return ans
+    
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(i, n):
+                cnt[s[j]] += 1
+                if len(set(cnt.values())) == 1:
+                    ans = max(ans, j - i + 1)
+        return ans
+
+# @lc code=end
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestBalanced("abbac"))
