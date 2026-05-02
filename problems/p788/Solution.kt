b34cf5a3050e994ff7c255d540eb9f9c63c528a8
mole828 <mrx800@163.com>
2026-05-02 10:53:12 +0800

    daily

---
+++ problems/p788/Solution.kt
@@ -0,0 +1,20 @@
+package problems.p788
+/*
+ * @lc app=leetcode id=788 lang=kotlin
+ *
+ * [788] Rotated Digits
+ */
+// @lc code=start
+class Solution {
+    fun isGood(n: Int) = 
+        n.toString().fold(false) { changed, c ->
+        when (c) {
+            '3', '4', '7' -> return false
+            '2', '5', '6', '9' -> true
+            else -> changed
+        }
+    }
+    fun rotatedDigits(n: Int) = (1..n).count { isGood(it) }
+}
+// @lc code=end
+
