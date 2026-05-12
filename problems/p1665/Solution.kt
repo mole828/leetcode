1311a59d94c1e9ba043af6497961b7101ad2b63a
mole828 <mrx800@163.com>
2026-05-12 10:14:50 +0800

    daily

---
+++ problems/p1665/Solution.kt
@@ -0,0 +1,18 @@
+package problems.p1665
+/*
+ * @lc app=leetcode id=1665 lang=kotlin
+ *
+ * [1665] Minimum Initial Energy to Finish Tasks
+ */
+
+// @lc code=start
+class Solution {
+    fun minimumEffort(tasks: Array<IntArray>): Int {
+        val tasks = tasks.map { it[0] to it[1] }.sortedByDescending { it.first - it.second }
+        return tasks.fold(0) { acc, (actual, minimum) ->
+            maxOf(acc + actual, minimum)
+        }
+    }
+}
+// @lc code=end
+
