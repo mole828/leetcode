84107b48f13154e6217e9432969e4d71f339d82a
mole828 <mrx800@163.com>
2026-05-22 10:32:20 +0800

    daily

---
+++ problems/p33/Solution.kt
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=33 lang=kotlin
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (43.96%)
+ * Likes:    30155
+ * Dislikes: 1836
+ * Total Accepted:    4.5M
+ * Total Submissions: 10M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * There is an integer array nums sorted in ascending order (with distinct
+ * values).
+ * 
+ * Prior to being passed to your function, nums is possibly left rotated at an
+ * unknown index k (1 <= k < nums.length) such that the resulting array is
+ * [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
+ * (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices
+ * and become [4,5,6,7,0,1,2].
+ * 
+ * Given the array nums after the possible rotation and an integer target,
+ * return the index of target if it is in nums, or -1 if it is not in nums.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * Example 3:
+ * Input: nums = [1], target = 0
+ * Output: -1
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 5000
+ * -10^4 <= nums[i] <= 10^4
+ * All values of nums are unique.
+ * nums is an ascending array that is possibly rotated.
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ */
+package p33
+// @lc code=start
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.lastIndex
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            if (nums[mid] == target) return mid
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+
+        return -1
+    }
+}
+// @lc code=end
