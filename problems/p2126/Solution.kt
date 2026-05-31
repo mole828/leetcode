d9d474f10727bf730e96dd38b2bca2e25daa5b62
mole828 <mrx800@163.com>
2026-05-31 14:01:31 +0800

    daily

---
+++ problems/p2126/Solution.kt
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=2126 lang=kotlin
+ *
+ * [2126] Destroying Asteroids
+ *
+ * https://leetcode.com/problems/destroying-asteroids/description/
+ *
+ * algorithms
+ * Medium (53.48%)
+ * Likes:    675
+ * Dislikes: 205
+ * Total Accepted:    95K
+ * Total Submissions: 169.1K
+ * Testcase Example:  '10\n[3,9,19,5,21]'
+ *
+ * You are given an integer mass, which represents the original mass of a
+ * planet. You are further given an integer array asteroids, where asteroids[i]
+ * is the mass of the i^th asteroid.
+ * 
+ * You can arrange for the planet to collide with the asteroids in any
+ * arbitrary order. If the mass of the planet is greater than or equal to the
+ * mass of the asteroid, the asteroid is destroyed and the planet gains the
+ * mass of the asteroid. Otherwise, the planet is destroyed.
+ * 
+ * Return true if all asteroids can be destroyed. Otherwise, return false.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: mass = 10, asteroids = [3,9,19,5,21]
+ * Output: true
+ * Explanation: One way to order the asteroids is [9,19,5,3,21]:
+ * - The planet collides with the asteroid with a mass of 9. New planet mass:
+ * 10 + 9 = 19
+ * - The planet collides with the asteroid with a mass of 19. New planet mass:
+ * 19 + 19 = 38
+ * - The planet collides with the asteroid with a mass of 5. New planet mass:
+ * 38 + 5 = 43
+ * - The planet collides with the asteroid with a mass of 3. New planet mass:
+ * 43 + 3 = 46
+ * - The planet collides with the asteroid with a mass of 21. New planet mass:
+ * 46 + 21 = 67
+ * All asteroids are destroyed.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: mass = 5, asteroids = [4,9,23,4]
+ * Output: false
+ * Explanation: 
+ * The planet cannot ever gain enough mass to destroy the asteroid with a mass
+ * of 23.
+ * After the planet destroys the other asteroids, it will have a mass of 5 + 4
+ * + 9 + 4 = 22.
+ * This is less than 23, so a collision would not destroy the last asteroid.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= mass <= 10^5
+ * 1 <= asteroids.length <= 10^5
+ * 1 <= asteroids[i] <= 10^5
+ * 
+ * 
+ */
+package p2126
+// @lc code=start
+class Solution {
+    fun asteroidsDestroyed(mass: Int, asteroids: IntArray): Boolean {
+        asteroids.sort()
+        var mass = mass.toLong()
+        asteroids.forEach { 
+            if (mass >= it) {
+                mass += it
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+}
+// @lc code=end
+
