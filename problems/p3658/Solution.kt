48495d084355b2ff93b475a3513e2b1a9d7c79ed
mole828 <mrx800@163.com>
2026-07-15 11:07:53 +0800

    daily

---
+++ problems/p3658/Solution.kt
@@ -0,0 +1,16 @@
+package p3658
+
+/*
+ * @lc app=leetcode id=3658 lang=kotlin
+ *
+ * [3658] GCD of Odd and Even Sums
+ */
+
+// @lc code=start
+class Solution {
+    fun gcdOfOddEvenSums(n: Int): Int {
+        return n
+    }
+}
+// @lc code=end
+
