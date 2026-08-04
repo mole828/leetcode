55d3a5c835981b7d2c8ea54c029119ed533f86f3
mole828 <mrx800@163.com>
2026-08-04 10:49:57 +0800

    daily

---
+++ problems/p3731/Solution.kt
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=3731 lang=kotlin
+ *
+ * [3731] Find Missing Elements
+ *
+ * https://leetcode.com/problems/find-missing-elements/description/
+ *
+ * algorithms
+ * Easy (83.30%)
+ * Likes:    112
+ * Dislikes: 5
+ * Total Accepted:    82K
+ * Total Submissions: 97.6K
+ * Testcase Example:  '[1,4,2,5]'
+ *
+ * You are given an integer array nums consisting of unique integers.
+ * 
+ * Originally, nums contained every integer within a certain range. However,
+ * some integers might have gone missing from the array.
+ * 
+ * The smallest and largest integers of the original range are still present in
+ * nums.
+ * 
+ * Return a sorted list of all the missing integers in this range. If no
+ * integers are missing, return an empty list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,4,2,5]
+ * 
+ * Output: [3]
+ * 
+ * Explanation:
+ * 
+ * The smallest integer is 1 and the largest is 5, so the full range should be
+ * [1,2,3,4,5]. Among these, only 3 is missing.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [7,8,6,9]
+ * 
+ * Output: []
+ * 
+ * Explanation:
+ * 
+ * The smallest integer is 6 and the largest is 9, so the full range is
+ * [6,7,8,9]. All integers are already present, so no integer is missing.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [5,1]
+ * 
+ * Output: [2,3,4]
+ * 
+ * Explanation:
+ * 
+ * The smallest integer is 1 and the largest is 5, so the full range should be
+ * [1,2,3,4,5]. The missing integers are 2, 3, and 4.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ * 
+ * 
+ */
+package p3731
+// @lc code=start
+class Solution {
+    fun findMissingElements(nums: IntArray): List<Int> {
+        var maxNum = nums.first()
+        var minNum = nums.first()
+        val numSet = mutableSetOf<Int>()
+        nums.forEach { num ->
+            if (num>maxNum) maxNum = num
+            if (num<minNum) minNum = num
+            numSet.add(num)
+        }
+        val lostList = mutableListOf<Int>()
+        (minNum..maxNum).forEach { num ->
+            if ((num in numSet).not()) lostList.add(num)
+        }
+        return lostList
+    }
+}
+// @lc code=end
+
