bf00b9fb5ffc82afebb6c0526d4b202137d16bb6
mole828 <mrx800@163.com>
2026-09-02 10:06:47 +0800

    daily

---
+++ problems/p3875/Solution.kt
@@ -0,0 +1,16 @@
+package p3875
+
+/*
+ * @lc app=leetcode id=3875 lang=kotlin
+ *
+ * [3875] Construct Uniform Parity Array I
+ */
+
+// @lc code=start
+class Solution {
+    fun uniformArray(nums1: IntArray): Boolean {
+        return true
+    }
+}
+// @lc code=end
+
